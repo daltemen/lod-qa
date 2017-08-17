82f0620df252d331bccc080b7bec67f92658d915
Daniel Mendoza <daltemen@hotmail.com>
2017-08-16 23:26:29 -0500

    Added textfiles and fix latency and scalability

--- sparql_process/evaluators.py
+++ sparql_process/evaluators.py
@@ -1,3 +1,4 @@
+import timeit
 from datetime import datetime
 
 from flogmodel.model import FuzzyLogicModel
@@ -25,29 +26,41 @@ class Evaluator:
 		timeliness = self.evaluate_timeliness()
 
 		quality_list = []
+		times_model = []
 		print("Evaluating Model-------->")
-		with open('QUALITY_OUTPUT.txt', 'w') as outfile:
-			for l,sc,sy,tr,ti in zip(
-				latency, scalability,
+		with open('QUALITY_OUTPUT.txt', 'w') as outfile, \
+			open('QUALITY_TIMES.txt', 'w') as quality_times:
+
+			for sy,tr,ti in zip(
 				syntactic_validaty, trustworthiness,
 				timeliness
 				):
+				start_model = timeit.default_timer()
 
-				fuzzy_logic = FuzzyLogicModel(l,
-							sc, sy,
-							tr, ti
+				fuzzy_logic = FuzzyLogicModel(
+							latency, scalability,
+							sy,	tr, ti
 							)
 				quality_list.append(fuzzy_logic.get_output())
 
+				final_model = timeit.default_timer()
+				time_model = final_model - start_model
+				times_model.append(time_model)
+
+			quality_times.write(','.join(map(str, times_model)))
 			outfile.write(','.join(map(str, quality_list)))
 
 	def evaluate_latency(self):
 		print("evaluating latency...")
 		self.performance.get_latency_times()
 		file = open('latency_times.txt', 'r')
-		result = file.read().split(',')
+		result = [float(x) for x in file.read().split(',')]
+		average_latency = sum(result) / float(len(result))
 
-		return result
+		with open('average_latency.txt', 'w') as outfile:
+			outfile.write(str(average_latency))
+
+		return average_latency
 
 	def evaluate_scalability(self):
 		print("evaluating scalability...")
@@ -58,21 +71,31 @@ class Evaluator:
 
 		list_of_lists = []
 		count = 0
-		for i in range(8):
-			count += 1
-			file = open('eight_users_{}.txt'.format(count),'r')
-			read = [float(x) for x in file.read().split(",")]
-			list_of_lists.append(read)
+		with open('result_scalability_averages.txt', 'w') as outfile:
 
-		sum_list = [sum(item) for item in zip(*list_of_lists)]
-		divide_list = [i/8 for i in sum_list]
-		result_list = [i/j for i,j in zip(result_one,divide_list)]
+			for i in range(8):
+				count += 1
+				file = open('eight_users_{}.txt'.format(count),'r')
+				read = [float(x) for x in file.read().split(",")]
+				list_of_lists.append(read)
 
-		return result_list
+			sum_list = [sum(item) for item in zip(*list_of_lists)]
+			divide_list = [float(i)/8 for i in sum_list]
+			result_list = [float(i)/j for i,j in zip(result_one,divide_list)]
+
+			outfile.write(','.join(map(str, result_list)))
+
+		average_scalability = sum(result_list) / float(len(result_list))
+
+		with open('average_scalability.txt', 'w') as outfile:
+			outfile.write(str(average_scalability))
+
+		return average_scalability
 
 	def evaluate_trustworthiness_from_dataset(self):
 		print("evaluating trustworthiness...")
 		with open('trustworthiness.txt', 'w') as outfile:
+
 			dataset_list = self.dataset.get_creators()
 			description_dataset = ''
 			trust_values_list = []
@@ -87,6 +110,7 @@ class Evaluator:
 	def evaluate_timeliness(self):
 		print("evaluating timeliness...")
 		with open('timeliness.txt', 'w') as outfile:
+
 			dataset_issue, dataset_modified = self.dataset.get_issued_and_modified()
 			result_list = []
 			# (current - modified) / (current - issued)
@@ -99,4 +123,5 @@ class Evaluator:
 						'%Y-%m-%d+%H:%M').strftime('%Y')))
 				)
 			outfile.write(','.join(map(str, result_list)))	
+
 		return result_list
